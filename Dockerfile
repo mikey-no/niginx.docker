e6968cc928edf491c1c6819276bcd4fa0ca1cc95
A Name <aemail.example.com>
2017-09-27 15:25:56 +0100

    working script to setup Docker nginx static web page html hello world page

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nginx
-RUN rm /etc/nginx/conf.d/default.conf
+#RUN rm /etc/nginx/conf.d/default.conf
 #RUN rm /etc/nginx/conf.d/example_ssl.conf
 COPY static-html-directory /usr/share/nginx/html
-COPY conf /etc/nginx
+#COPY conf /etc/nginx
--- go.bat
+++ go.bat
@@ -6,17 +6,27 @@ rem docker run --name mynginx1my -P -d nginx
 
 set image=mynginximage
 set tag=lastest
-set conrun=mynginx3xx
+set conrun=mynginx3
 
+echo Start by cleaning up...
+docker rmi -f %image%
+docker stop %conrun%
+docker rm %conrun%
+
+echo Build the image...
 docker build -t %image% .
 
+echo Run the image...
 docker run --name %conrun% -P -d %image%
+
+echo Show the running containers...
 docker ps
 
 rem docker exec -it [container-id] bash
 
-echo "Cleanup"
+echo Cleanup....
 pause
 
 docker stop %conrun%
+docker rm %conrun%
 docker rmi -f %image%
